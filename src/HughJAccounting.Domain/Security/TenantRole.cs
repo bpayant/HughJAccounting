b55ca11bca4c52720c64084405eaa8b02e579239
Brian <hocki101@hotmail.com>
2026-07-02 19:16:58 -0500

    Add Identity package for security foundation

---
+++ src/HughJAccounting.Domain/Security/TenantRole.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HughJAccounting.Domain.Security
+{
+    internal class TenantRole
+    {
+    }
+}
---
+++ src/HughJAccounting.Infrastructure/Identity/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HughJAccounting.Infrastructure.Identity
+{
+    internal class ApplicationUser
+    {
+    }
+}
